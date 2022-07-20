9338927732a814d6de4fcfc9f51a26b9cf2d1eb5
DANIELNAHUN <nahuncf5@gmail.com>
2022-07-19 23:57:24 -0500

    add styles v4

--- components/Counter.js
+++ components/Counter.js
@@ -5,11 +5,25 @@ Vue.component('counter', {
         <button @click="counterNumber++">+</button>
         <h3>{{counterNumber}}</h3>
         <button @click="counterNumber--">--</button>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" :style="{width: counterNumber+'%'}"
+            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"
+            :class="color">{{counterNumber}}%</div>
+        </div>
     </div>
     `,
     data () {
         return {
             counterNumber: 0
+        }
+    },
+    computed :{
+        color () {
+            return {
+                'bg-success': this.counterNumber <= 10,
+                'bg-warning': this.counterNumber > 10 && this.counterNumber < 20,
+                'bg-danger': this.counterNumber >= 20
+            }
         }
     }
 
